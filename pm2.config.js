0b1d3f1d795c47788ec29a872c38048509d1ae40
pencilbsp <pencil.bsp@gmail.com>
2024-01-16 10:38:27 +0700

    add pm2 config

---
+++ pm2.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: "fuhuzz.rip backend", // Name of your application
+  script: "src/app.ts", // Entry point of your application
+  interpreter: "bun", // Path to the Bun interpreter
+}
