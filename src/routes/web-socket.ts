34e98e57ae8eac339e09cdbc4521fea8a324308e
pencilbsp <pencil.bsp@gmail.com>
2024-05-15 11:11:50 +0700

    skip new QR code

--- src/routes/web-socket.ts
+++ src/routes/web-socket.ts
@@ -1,25 +1,28 @@
-import sharp from "sharp"
-import { $, file } from "bun"
-import { existsSync } from "fs"
-import { Elysia } from "elysia"
-import { randomUUID } from "crypto"
-import { join, dirname } from "path"
+import sharp from "sharp";
+import { $, file } from "bun";
+import { existsSync } from "fs";
+import { Elysia } from "elysia";
+import { randomUUID } from "crypto";
+import { join, dirname } from "path";
 
-import { STATIC_DIR } from "@/configs"
-import { decode } from "@/utils/base64"
-import myEmitter from "@/utils/emiter"
-import { decrypt } from "@/utils/fuhu/client"
-import decryptImages from "@/utils/fuhu/decrypt-images"
+import { STATIC_DIR } from "@/configs";
+import { decode } from "@/utils/base64";
+import myEmitter from "@/utils/emiter";
+import { decrypt } from "@/utils/fuhu/client";
+import decryptImages from "@/utils/fuhu/decrypt-images";
 
 function getFid(urlBase64?: string) {
-  if (!urlBase64) return null
+  if (!urlBase64) return null;
   try {
-    const pathname = new URL(decode(urlBase64)).pathname
-    const slug = pathname.split("/")[2].replaceAll("_", "/").replaceAll("-", "+") + "=="
-    const payload = decrypt(slug, "--KpQG3w0km3imY", "b63e541bc9ece19a").split("|")
-    return payload[0]
+    const pathname = new URL(decode(urlBase64)).pathname;
+    const slug =
+      pathname.split("/")[2].replaceAll("_", "/").replaceAll("-", "+") + "==";
+    const payload = decrypt(slug, "--KpQG3w0km3imY", "b63e541bc9ece19a").split(
+      "|"
+    );
+    return payload[0];
   } catch (error) {
-    return null
+    return null;
   }
 }
 
@@ -39,60 +42,68 @@ function getFid(urlBase64?: string) {
 
 const sourceLogger = async (url: string, source: string) => {
   try {
-    const sourceDir = join(STATIC_DIR, "source")
-    if (!existsSync(sourceDir)) await $`mkdir -p ${sourceDir}`
+    const sourceDir = join(STATIC_DIR, "source");
+    if (!existsSync(sourceDir)) await $`mkdir -p ${sourceDir}`;
 
-    const htmlPath = join(sourceDir, randomUUID())
+    const htmlPath = join(sourceDir, randomUUID());
 
-    await Bun.write(htmlPath, `<!-- ${Date.now()} -->\n<!-- ${url} -->\n${source}`)
+    await Bun.write(
+      htmlPath,
+      `<!-- ${Date.now()} -->\n<!-- ${url} -->\n${source}`
+    );
   } catch (error) {}
-}
+};
 
 const webSocket = new Elysia().ws("/ws", {
   async message(_, message: any) {
     try {
       if (Buffer.isBuffer(message)) {
-        const payloadLengthArray = new Uint8Array(message.slice(0, 4))
-        const payloadLength = new Uint32Array(payloadLengthArray.buffer)[0]
+        const payloadLengthArray = new Uint8Array(message.slice(0, 4));
+        const payloadLength = new Uint32Array(payloadLengthArray.buffer)[0];
 
         // Extract the URL data based on the length
-        const playloadArray = new Uint8Array(message.slice(4, 4 + payloadLength))
-        const payload = JSON.parse(new TextDecoder().decode(playloadArray))
-        const blobData = message.slice(4 + payloadLength)
+        const playloadArray = new Uint8Array(
+          message.slice(4, 4 + payloadLength)
+        );
+        const payload = JSON.parse(new TextDecoder().decode(playloadArray));
+        const blobData = message.slice(4 + payloadLength);
 
         if (payload.type === "image") {
-          const image = sharp(blobData)
-          const metadata = await image.metadata()
+          const image = sharp(blobData);
+          const metadata = await image.metadata();
 
-          if (metadata.width === 450 && metadata.height === 450) {
-            console.log("Skip QR code image")
+          if (metadata.width === 300 && metadata.height === 300) {
+            console.log("Skip QR code image");
+          } else if (metadata.width === 450 && metadata.height === 450) {
+            console.log("Skip QR code image");
           } else {
-            console.log(payload.url)
-            const imagePath = join(STATIC_DIR, "images", payload.url)
-            if (!existsSync(dirname(imagePath))) await $`mkdir -p ${dirname(imagePath)}`
+            console.log(payload.url);
+            const imagePath = join(STATIC_DIR, "images", payload.url);
+            if (!existsSync(dirname(imagePath)))
+              await $`mkdir -p ${dirname(imagePath)}`;
 
-            await image.toFile(imagePath)
+            await image.toFile(imagePath);
           }
 
-          image.destroy()
+          image.destroy();
         }
 
         if (payload.type === "segment") {
-          myEmitter.emit(payload.url, blobData)
+          myEmitter.emit(payload.url, blobData);
         }
       } else {
-        const { action, payload } = message
+        const { action, payload } = message;
 
         /**
          * Lưu m3u8 file
          */
         if (action === "m3u8_content") {
-          const videoDir = join(STATIC_DIR, "m3u8", payload.fid)
-          const m3u8Path = join(videoDir, "index.m3u8")
+          const videoDir = join(STATIC_DIR, "m3u8", payload.fid);
+          const m3u8Path = join(videoDir, "index.m3u8");
 
           if (!existsSync(m3u8Path)) {
-            await $`mkdir -p ${videoDir}`
-            await Bun.write(m3u8Path, payload.data)
+            await $`mkdir -p ${videoDir}`;
+            await Bun.write(m3u8Path, payload.data);
           }
         }
 
@@ -100,16 +111,24 @@ const webSocket = new Elysia().ws("/ws", {
          * Lưu hls key
          */
         if (action === "hls_key") {
-          const m3u8Path = join(STATIC_DIR, "m3u8", payload.fid, "index.m3u8")
-          const f = file(m3u8Path)
-          const m3u8Content = await f.text()
+          const m3u8Path = join(STATIC_DIR, "m3u8", payload.fid, "index.m3u8");
+          const f = file(m3u8Path);
+          const m3u8Content = await f.text();
 
-          const base64Key = Buffer.from(payload.keyHex, "hex").toString("base64")
-          await Bun.write(m3u8Path, m3u8Content.replaceAll(payload.keyUrl, `data:text/plain;base64,${base64Key}`))
+          const base64Key = Buffer.from(payload.keyHex, "hex").toString(
+            "base64"
+          );
+          await Bun.write(
+            m3u8Path,
+            m3u8Content.replaceAll(
+              payload.keyUrl,
+              `data:text/plain;base64,${base64Key}`
+            )
+          );
         }
 
         if (action === "log_source") {
-          await sourceLogger(payload.url, payload.source)
+          await sourceLogger(payload.url, payload.source);
           // console.log("===========[Log source]===========")
           // console.log("[+] URL:", payload.url)
           // console.log("[+] SOURCE:", payload.source)
@@ -117,28 +136,28 @@ const webSocket = new Elysia().ws("/ws", {
         }
 
         if (action === "log_images") {
-          await decryptImages(payload.fid, { data: payload.data })
+          await decryptImages(payload.fid, { data: payload.data });
         }
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   },
   close(ws, code, message) {
-    console.log("A client disconnected!")
+    console.log("A client disconnected!");
     // myEmitter.off("load_segment", (data) => loadSegment(ws, data))
   },
   async open(ws) {
-    const fid = getFid(ws.data.query.url)
+    const fid = getFid(ws.data.query.url);
     // myEmitter.on("load_segment", (data) => loadSegment(ws, data))
 
     if (fid !== null) {
-      const imageDir = join(STATIC_DIR, "images", fid)
-      if (!existsSync(imageDir)) await $`mkdir -p ${imageDir}`
+      const imageDir = join(STATIC_DIR, "images", fid);
+      if (!existsSync(imageDir)) await $`mkdir -p ${imageDir}`;
     }
 
-    console.log("A client connected!")
+    console.log("A client connected!");
   },
-})
+});
 
-export default webSocket
+export default webSocket;
